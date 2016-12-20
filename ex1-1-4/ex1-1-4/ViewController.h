49c6a97187d0b736aa21ed3ae678eb9070e2db70
ITユーザー <it-user@kpc-it3166mc.kmt.neec.ac.jp>
2016-12-20 11:05:40 +0900

    @MGR9029 #4 start

---
+++ ex1-1-4/ex1-1-4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ex1-1-4
+//
+//  Created by ITユーザー on 2016/12/20.
+//  Copyright © 2016年 富樫克仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
