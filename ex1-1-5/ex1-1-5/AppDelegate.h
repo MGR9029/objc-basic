5f706ba4bc909ccd3f6a79dd812055b67aa53779
ITユーザー <it-user@kpc-it3166mc.kmt.neec.ac.jp>
2016-12-22 13:47:26 +0900

    @MGR9029 #5 start

---
+++ ex1-1-5/ex1-1-5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ex1-1-5
+//
+//  Created by ITユーザー on 2016/12/22.
+//  Copyright © 2016年 富樫克仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ex1-1-5/ex1-1-5/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ex1-1-5
+//
+//  Created by ITユーザー on 2016/12/22.
+//  Copyright © 2016年 富樫克仁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
