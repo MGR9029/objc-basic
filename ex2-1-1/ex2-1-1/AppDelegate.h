d1e75f177201bea145b2d94ef2c6e411517858db
ITユーザー <it-user@kpc-it3166mc.kmt.neec.ac.jp>
2017-01-12 10:17:56 +0900

    @MGR9029 #7 start

---
+++ ex2-1-1/ex2-1-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ex2-1-1
+//
+//  Created by ITユーザー on 2017/01/12.
+//  Copyright © 2017年 富樫克仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ex2-1-1/ex2-1-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ex2-1-1
+//
+//  Created by ITユーザー on 2017/01/12.
+//  Copyright © 2017年 富樫克仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
