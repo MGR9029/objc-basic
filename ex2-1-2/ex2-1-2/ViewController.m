18c0d4c18333eb9479fa2bc55d89f92b3186db22
ITユーザー <it-user@kpc-it3166mc.kmt.neec.ac.jp>
2017-01-12 11:58:53 +0900

    @MGR9029 #8 start

---
+++ ex2-1-2/ex2-1-2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ex2-1-2
+//
+//  Created by ITユーザー on 2017/01/12.
+//  Copyright © 2017年 富樫克仁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
