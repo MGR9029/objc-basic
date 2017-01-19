21ec14d4ccc98f67108c9aaba888fafc93fc6d85
ITユーザー <it-user@kpc-it3166mc.kmt.neec.ac.jp>
2017-01-19 10:02:53 +0900

    @MGR9029 #9 start

---
+++ ex2-1-3/ex2-1-3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ex2-1-3
+//
+//  Created by ITユーザー on 2017/01/19.
+//  Copyright © 2017年 富樫克仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
